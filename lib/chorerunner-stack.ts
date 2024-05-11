609fedd535b166420572419395610ab3d40990b9
Daniel Nichols <herrnichols@gmaill.com>
2024-05-11 03:26:54 -0400

    feat: add test lambda

--- lib/chorerunner-stack.ts
+++ lib/chorerunner-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
+import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
+import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ChoreRunnerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const gateway = new RestApi(this, 'chorerunner-api', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkappQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloLambda = new NodejsFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '..', 'src', 'helloLambda', 'index.ts'),
+      handler: 'index.handler'
+    })
+
+
+    const helloPath = gateway.root.addResource('hello').addMethod('ANY', new LambdaIntegration(helloLambda))
+
   }
 }
