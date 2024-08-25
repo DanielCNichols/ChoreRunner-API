fdde2605ca77903c5c4f0d61589075d50a5215b8
Daniel Nichols <herrnichols@gmaill.com>
2024-08-24 23:17:20 -0400

    chore: remove express WIP

--- src/dynamo/dynamoservice.ts
+++ src/dynamo/dynamoservice.ts
@@ -1,5 +1,4 @@
 import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
-import { Endpoint } from "aws-cdk-lib/aws-docdb/lib/endpoint";
 
 const config: DynamoDBClientConfig = {}
 
